8bef4812275b556b2a2f4b1eb9f0f857bda69af6
KavinnatRaw <150155430+noellnon@users.noreply.github.com>
2023-11-21 19:24:24 +0700

    push

--- Frontend/User/src/App.jsx
+++ Frontend/User/src/App.jsx
@@ -8,7 +8,6 @@ import UserFinishPF from './UserFinishPF'
 import ChatContainer from './Chat/ChatContainer'
 import TerminateMate from './TerminateMate'
 import Dashboard from './Dashboard'
-import ThankYou from './ThankYou'
 import { Cookies, useCookies } from 'react-cookie'
 
 function App() {
@@ -23,13 +22,12 @@ function App() {
         <Route path='Signin' element={<UserSignin/>}></Route> 
         <Route path='Signup' element={<UserSignup/>}></Route> 
         <Route path='Register' element={<UserRegister/>}></Route>
-        <Route path='Agreement' element={<UserAgreement/>}></Route>
-        <Route path='Chat' element={<ChatContainer/>}></Route>
-        <Route path='TerminateMate' element={<TerminateMate/>}></Route>
         <Route path='Mode' element={<UserMode/>}></Route>
         <Route path='Profile' element={<UserFinishPF/>}></Route>
+        <Route path='Agreement' element={<UserAgreement/>}></Route>
         <Route path='Home' element={<Dashboard/>}></Route>
-        <Route path='ThankYou' element={<ThankYou/>}></Route>
+        <Route path='Chat' element={<ChatContainer/>}></Route>
+        <Route path='TerminateMate' element={<TerminateMate/>}></Route>
         </Routes>
     </div>
   )
